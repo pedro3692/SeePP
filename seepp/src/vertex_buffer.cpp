706c084ccfa61b1b130b93b67a5dbf41cf21918c
Pedro Perdido <pedro3692@gmail.com>
2023-12-17 20:27:42 +0000

    started vertex shader

--- seepp/src/vertex_buffer.cpp
+++ seepp/src/vertex_buffer.cpp
@@ -2,8 +2,16 @@
 
 #include "vertex_buffer.h"
 
-VertexBuffer::VertexBuffer()
-{
-    glGenBuffers(1, &m_id);
-    glBindBuffer(GL_ARRAY_BUFFER, m_id);
+namespace SeePP {
+
+VertexBuffer::VertexBuffer() {
+  glGenBuffers(1, &m_id);
+  glBindBuffer(GL_ARRAY_BUFFER, m_id);
 }
+
+void VertexBuffer::BindData(float *vertices, DrawMode mode)
+{
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, (GLenum)mode);
+}
+
+} // namespace SeePP
--- seepp/src/vertex_buffer.h
+++ seepp/src/vertex_buffer.h
@@ -2,15 +2,27 @@
 #define VERTEX_BUFFER_H_
 
 #include <cstdint>
+#include <glad/gl.h>
+
+namespace SeePP {
+
+enum class DrawMode {
+  None,
+  Stream = GL_STREAM_DRAW,
+  Static = GL_STATIC_DRAW,
+  Dynamic = GL_DYNAMIC_DRAW,
+};
 
 class VertexBuffer {
 public:
-    VertexBuffer();
-    ~VertexBuffer();
+  VertexBuffer();
+  ~VertexBuffer();
 
-    void SetData(float* vertices);
+  void BindData(float *vertices, DrawMode mode);
+
 private:
-    uint32_t m_id = 0;
+  uint32_t m_id = 0;
 };
 
+} // namespace SeePP
 #endif // VERTEX_BUFFER_H_
